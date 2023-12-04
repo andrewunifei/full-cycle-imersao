094e092e441f5c9e7f374153d91eeaaf95e06fce
Andrew <andrewunifei@gmail.com>
2023-12-04 12:41:55 -0300

    [ADD] Entity Asset, Order, & Transaction

--- internal/market/entity/investor.go
+++ internal/market/entity/investor.go
@@ -1,14 +1,15 @@
 package entity
 
 type Investor struct {
-	ID string
-	Name string
-	AssetPosition []*InvestorAssetPosition
+	ID 				string
+	Name 			string
+	AssetPosition	[]*InvestorAssetPosition
 }
 
-func NewInvestor(id string) *Investor {
+func NewInvestor(id string, name string) *Investor {
 	return &Investor{
 		ID: id,
+		Name: name,
 		AssetPosition: []*InvestorAssetPosition{},
 	}
 }
